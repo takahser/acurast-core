231bce1da27f091a1ebc098ab7b114294a36686c
Simon Wehrli <simon@smartnuance.com>
2023-01-24 20:50:42 +0100

    tmp: not working genesis for pallet_acurast_assets

--- pallets/assets/src/lib.rs
+++ pallets/assets/src/lib.rs
@@ -10,7 +10,6 @@
 // mod benchmarking;
 pub mod weights;
 
-use frame_support::{dispatch::Weight, traits::Get};
 use sp_runtime::traits::StaticLookup;
 
 pub use pallet::*;
@@ -24,7 +23,7 @@ pub mod pallet {
     use super::*;
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
-    use xcm::latest::MultiLocation;
+    use xcm::prelude::AssetId;
 
     #[pallet::pallet]
     #[pallet::generate_store(pub(super) trait Store)]
@@ -40,15 +39,37 @@ pub mod pallet {
         type WeightInfo: WeightInfo;
     }
 
+    #[pallet::genesis_config]
+    pub struct GenesisConfig<T: Config<I>, I: 'static = ()> {
+        /// Genesis assets: internal asset ID -> asset ID
+        pub assets: Vec<(<T as pallet_assets::Config<I>>::AssetId, AssetId)>,
+    }
+
+    #[cfg(feature = "std")]
+    impl<T: Config<I>, I: 'static> Default for GenesisConfig<T, I> {
+        fn default() -> Self {
+            Self{
+                assets: Default::default(),
+            }
+        }
+    }
+
+    #[pallet::genesis_build]
+    impl<T: Config<I>, I: 'static> GenesisBuild<T, I> for GenesisConfig<T, I> {
+        fn build(&self) {
+            // TODO
+        }
+    }
+
     #[pallet::storage]
     #[pallet::getter(fn asset_index)]
     pub type AssetIndex<T: Config<I>, I: 'static = ()> =
-        StorageMap<_, Blake2_128, <T as pallet_assets::Config<I>>::AssetId, MultiLocation>;
+        StorageMap<_, Blake2_128, <T as pallet_assets::Config<I>>::AssetId, AssetId>;
 
     #[pallet::storage]
     #[pallet::getter(fn reverse_asset_index)]
     pub type ReverseAssetIndex<T: Config<I>, I: 'static = ()> =
-        StorageMap<_, Blake2_128, MultiLocation, <T as pallet_assets::Config<I>>::AssetId>;
+        StorageMap<_, Blake2_128, AssetId, <T as pallet_assets::Config<I>>::AssetId>;
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -69,7 +90,7 @@ pub mod pallet {
     where
         T: pallet_assets::Config<I>,
     {
-        /// Creates and indexes a bijective mapping `id <-> native_id` and proxies to [`pallet_assets::Pallet::force_create()`].
+        /// Creates and indexes a bijective mapping `id <-> internal_id` and proxies to [`pallet_assets::Pallet::force_create()`].
         ///
         /// This extrinsic is idempotent when used with the same `id` and `asset` (does not receate the asset in `pallet_asset`.
         /// Trying to index an already indexed asset or using the same id to index a different asset results in an error.
@@ -78,7 +99,7 @@ pub mod pallet {
         pub fn force_create(
             origin: OriginFor<T>,
             id: <T as pallet_assets::Config<I>>::AssetIdParameter,
-            asset: MultiLocation,
+            asset: AssetId,
             owner: AccountIdLookupOf<T>,
             is_sufficient: bool,
             min_balance: T::Balance,
@@ -116,7 +137,7 @@ pub mod pallet {
         #[pallet::weight(<T as Config<I>>::WeightInfo::set_metadata(name.len() as u32, symbol.len() as u32))]
         pub fn set_metadata(
             origin: OriginFor<T>,
-            id: MultiLocation,
+            id: AssetId,
             name: Vec<u8>,
             symbol: Vec<u8>,
             decimals: u8,
@@ -129,7 +150,7 @@ pub mod pallet {
         #[pallet::weight(<T as Config<I>>::WeightInfo::transfer())]
         pub fn transfer(
             origin: OriginFor<T>,
-            id: MultiLocation,
+            id: AssetId,
             target: AccountIdLookupOf<T>,
             #[pallet::compact] amount: T::Balance,
         ) -> DispatchResult {
@@ -141,7 +162,7 @@ pub mod pallet {
         #[pallet::weight(<T as Config<I>>::WeightInfo::force_transfer())]
         pub fn force_transfer(
             origin: OriginFor<T>,
-            id: MultiLocation,
+            id: AssetId,
             source: AccountIdLookupOf<T>,
             dest: AccountIdLookupOf<T>,
             #[pallet::compact] amount: T::Balance,
